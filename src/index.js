52d58323208a3db64508be2e4a031fc45321127b
Italo1994 <italohortiz2015@gmail.com>
2024-06-24 06:19:15 -0300

    feat: create the 2 characters

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const PLAYER1 = {
+    NAME: "Mario",
+    SPEED: 4,
+    MANEUVERABILITY: 3,
+    POWER: 3,
+    POINTS: 0
+};
+
+const PLAYER2 = {
+    NAME: "Luigi",
+    SPEED: 3,
+    MANEUVERABILITY: 4,
+    POWER: 4,
+    POINTS: 0
+};
+
